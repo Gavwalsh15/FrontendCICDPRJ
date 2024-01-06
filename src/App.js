7b34c840bb8462243c964b4da1180be4dbdf03cf
RakibR7 <G00408644@ATU.IE>
2024-01-06 17:26:01 +0000

    Finally fixed Checkout page, but broke login page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import './App.css';
 import Signup from './Signup/Signup';
 import Header from "./Header";
 import Home from "./Home";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-/*
 function App() {
     return (
-        // BEM
         <Router>
             <div className="App">
                 <h1>Sign Up</h1>
@@ -19,29 +16,6 @@ function App() {
                     <Route path="/checkout" element={[<Header />, <h1>I am a checkout page</h1>]}/>
                     <Route path="/" element={[<Header />, <Home />]}/>
                 </Routes>
-            </div>
-        </Router>
-    );
-}
-
-export default App;
-
-*/
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route path="/checkout" exact>
-                        <Header />
-                        <h1> I am checkout </h1>
-                    </Route>
-                    <Route path="/" exact>
-                        <Header />
-                        <Home />
-                    </Route>
-                </Switch>
             </div>
         </Router>
     );
