900d2e20b1d456e2c515a92ce5a8dbb2fa806068
Gavwalsh15 <114908190+Gavwalsh15@users.noreply.github.com>
2024-01-07 01:15:36 +0000

    Add files via upload

---
+++ src/Product/Product.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./Product.css"
+
+function Product({seller, id, title, image, price, rating}) {
+    return <div
+        className= "product" >
+        <div className="product_details">
+            <p> {title}</p>
+            <p className="product_price">
+                <small>$</small>
+                <strong>{price}</strong>
+            </p>
+
+        </div>
+        <img src={image}
+        />
+
+        <button>Buy Now</button>
+    </div>;
+}
+
+export default Product;
